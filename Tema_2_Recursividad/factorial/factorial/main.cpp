8628c4ea6e8e0522f9e8484b7ec9096997fafe61
Vicente Cubells <vcubells@gmail.com>
2020-08-14 08:38:45 -0500

    Se adicionaron ejemplos de recursividad
    - Se adicionó el ejemplo de factorial
    - Se adicionó el ejemplo de potencia

---
+++ Tema_2_Recursividad/factorial/factorial/main.cpp
@@ -0,0 +1,76 @@
+//
+//  main.cpp
+//  factorial
+//
+//  Created by Vicente Cubells Nonell on 13/08/20.
+//  Copyright © 2020 Vicente Cubells Nonell. All rights reserved.
+//
+
+#include <iostream>
+
+long int factorial(int n)
+{
+    /* Condición de parada */
+    if (n == 0) {
+        return 1;
+    }
+    /* Paso recursivo */
+    else {
+        return n * factorial(n-1);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    
+    int n = -1;
+    
+    /* Entrar el número a calcular el factorial */
+    do {
+        std::cout << "Entre el número: ";
+        std::cin >> n;
+    }
+    while (n < 0);
+    
+    /* Calcular el factorial de n */
+    long int resultado = factorial(n);
+    
+    /* Mostrar el resultado */
+    std::cout << "El factorial de " << n << " = " << resultado << std::endl;
+    
+    return 0;
+}
+
+/*
+ 
+n = 5   factorial(5)   5 * factorial(5-1) =     // Paso recursivo
+n = 4   factorial(4)   4 * factorial(4-1) =     // Paso recursivo
+n = 3   factorial(3)   3 * factorial(3-1) =     // Paso recursivo
+n = 2   factorial(2)   2 * factorial(2-1) =     // Paso recursivo
+n = 1   factorial(1)   1 * factorial(1-1) =     // Paso recursivo
+n = 0   factorial(0)                      = 1   // Condición de parada
+ 
+n = 1   factorial(1)   1 * 1 = 1
+n = 2   factorial(2)   2 * 1 = 2
+n = 3   factorial(3)   3 * 2 = 6
+n = 4   factorial(4)   4 * 6 = 24
+n = 5   factorial(5)   5 * 24 = 120
+ 
+ = factorial(5)
+ = factorial( 5 * factorial(4) )
+ = factorial( 5 * factorial( 4 * factorial(3) ) )
+ = factorial( 5 * factorial( 4 * factorial( 3 * factorial(2) ) ) )
+ = factorial( 5 * factorial( 4 * factorial( 3 * factorial( 2 * factorial(1) ) ) ) )
+ = factorial( 5 * factorial( 4 * factorial( 3 * factorial( 2 * factorial( 1 * factorial(0) ) ) ) ) )
+ 
+ = factorial( 5 * factorial( 4 * factorial( 3 * factorial( 2 * factorial( 1 * 1 ) ) ) ) )
+ = factorial( 5 * factorial( 4 * factorial( 3 * factorial( 2 * 1 ) ) ) )
+ = factorial( 5 * factorial( 4 * factorial( 3 * 2 ) ) )
+ = factorial( 5 * factorial( 4 * 6 ) )
+ = factorial( 5 * 24 )
+ = 120
+ 
+*/
+
+
+
+
---
+++ Tema_2_Recursividad/potencia/potencia/main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//  potencia
+//
+//  Created by Vicente Cubells Nonell on 14/08/20.
+//  Copyright © 2020 Vicente Cubells Nonell. All rights reserved.
+//
+
+#include <iostream>
+
+long int potencia(int x, int n)
+{
+    /* Condición de parada */
+    if (n == 0) {
+        std::cout << "Llegué a la condición de parada" << std::endl;
+        return 1;
+    }
+    /* Paso recursivo */
+    else {
+        std::cout << "Calculando " << x <<"^" << n << std::endl;
+        long int pot = potencia(x, n-1);
+        long int res = x * pot;
+        std::cout << "Resultado " << x <<"^" << n << " = " << res << std::endl;
+        return res;
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    int n = -1;
+    int x = -1;
+    
+    /* Entrar el número x a evelar a la potencia n */
+    do {
+        std::cout << "Entre el número: ";
+        std::cin >> x;
+    }
+    while (x <= 0);
+    
+    /* Entrar la potencia n */
+    do {
+        std::cout << "Entre la potencia: ";
+        std::cin >> n;
+    }
+    while (n < 0);
+    
+    /* Calcular la potencia de x a la n */
+    long int resultado = potencia(x, n);
+    
+    /* Mostrar el resultado */
+    std::cout << x << "^" << n << " = " << resultado << std::endl;
+    
+    return 0;
+}
