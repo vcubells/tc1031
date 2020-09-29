a3ceae2a2d846b14a5cccf31a06bf45c4ded3e66
Vicente Cubells <vcubells@gmail.com>
2020-09-29 17:17:03 -0500

    Se adicionaron métodos a la clase LinkedList<T>
    Clase LinkedList<T>
    
    * Se adicionó el método insert()
    * Se adicionó el método insert_back()
    * Se adicionó el método remove()
    * Se adicionó el método remove_back()
    * Se adicionó el método clear()
    * Se adionó el método at()
    * Se adicionó el método index()
    
    main
    
    * Se adicionaron ejemplos de como utilizar los métodos de la clase LinkedList<>

--- Tema_7_Estructuras lineales/LinkedList/LinkedList/LinkedList.hpp
+++ Tema_7_Estructuras lineales/LinkedList/LinkedList/LinkedList.hpp
@@ -63,11 +63,14 @@ public:
     /* Eliminar todos los elementos de la lista y liberar la memoria ocupada */
     virtual void clear();
     
-    /* Obtener el elemento que se encuentra en una posición */
+    /* Obtener el nodo que se encuentra en una posición */
     Node<T> * at(int) const;
     
     /* Obtener la posición de un nodo */
-    int at(Node<T> *) const;
+    int index(Node<T> *) const;
+    
+    /* Obtener la posición de un valor */
+    int index(const T &) const;
 
     /* Mostrar el contenido de la lista */
     template <typename Tn>
@@ -104,110 +107,226 @@ bool LinkedList<T>::empty() const
 
 /* Insertar un elemento en una posición dada */
 template <class T>
-void LinkedList<T>::insert(const T &, int)
+void LinkedList<T>::insert(const T & value, int position)
 {
-    /* Por implementar */
+    /* Crear el nuevo nodo a insertar */
+    Node<T> * newnode = new Node<T>(value);
+    
+    this->insert(newnode, position);
 }
-
+/* Insertar un nodo en una posición dada
+ * Si position < 0 se inserta al inicio
+ * Si position > _size se inserta al final
+ * en cualquier otro caso, se inserta en la posición dada
+ */
 template <class T>
-void LinkedList<T>::insert(Node<T> * node, int)
+void LinkedList<T>::insert(Node<T> * node, int position)
 {
-    /* Por implementar */
-}
-
-/* Insertar un elemento al inicio */
-template <class T>
-void LinkedList<T>::insert_front(const T & value)
-{
-    /* Crear un nuevo nodo */
-    Node<T> * node = new Node<T>(value);
-    this->insert_front(node);
-}
-
-template <class T>
-void LinkedList<T>::insert_front(Node<T> * node)
-{
-    /* Insertar el primer elemento */
-    if (this->empty()) {
-        this->_first = node;
-    }
-    /* Insertar cualquier elemento */
-    else {
+    /* Cuando la lista está vacía o position < 0 se inserta al inicio */
+    if (this->empty() || position <= 0) {
         node->setNext(this->_first);
         this->_first = node;
+    }
+    /* Cuando se inserta en cualquier posición diferente del inicio */
+    else {
+        
+        /* Si position > size se inserta al final */
+        if (position > this->_size) { position = this->_size; }
+        
+        /* Obtener el nodo que está en la posición anterior */
+        Node<T> * previous = this->at(position-1);
+        
+        /* Actualizar apuntadores */
+        node->setNext(previous->getNext());
+        previous->setNext(node);
     }
     
     /* Incrementar el tamaño de la lista */
     ++this->_size;
 }
 
-/* Insertar un elemento al final */
+/* Insertar un elemento al inicio */
 template <class T>
-void LinkedList<T>::insert_back(const T &)
+void LinkedList<T>::insert_front(const T & value)
 {
-    /* Por implementar */
+    this->insert(value, 0);
 }
 
 template <class T>
-void LinkedList<T>::insert_back(Node<T> *)
+void LinkedList<T>::insert_front(Node<T> * node)
 {
-    /* Por implementar */
+    this->insert(node, 0);
+}
+
+/* Insertar un elemento al final */
+template <class T>
+void LinkedList<T>::insert_back(const T & value)
+{
+    this->insert(value, this->_size);
+}
+
+template <class T>
+void LinkedList<T>::insert_back(Node<T> * node)
+{
+    this->insert(node, this->_size);
 }
 
 /* Eliminar el elemento en la posición dada */
 template <class T>
-Node<T> * LinkedList<T>::remove(int)
+Node<T> * LinkedList<T>::remove(int position)
 {
-    /* Por implementar */
+    /* Cuando la lista está vacía o position es inválida */
+    if (this->empty() || (position < 0 || position >= this->_size )) {
+        return nullptr;
+    }
+
+    /* Declarar variable temporal para guardar el nodo a eliminar */
+    Node<T> * removenode;
+    
+    /* Eliminar el primer nodo de la lista */
+    if (position == 0) {
+        /* Se guarda el apuntador a first */
+        removenode = this->_first;
+        
+        /* Se actualiza first al siguiente nodo */
+        this->_first = this->_first->getNext();
+        
+    }
+    /* Eliminar cualquier otro nodo */
+    else {
+        /* Se obtiene el apuntador al anterior */
+        Node<T> * previous = this->at(position-1);
+        
+        /* Se guarda el apuntador al nodo a eliminar */
+        removenode = previous->getNext();
+        
+        /* Se actualiza el anterior para que apunte al siguiente nodo */
+        previous->setNext(removenode->getNext());
+    }
+
+    /* Establecer el siguiente en nullptr */
+    removenode->setNext(nullptr);
+    
+    /* Decrementar el tamaño de la lista */
+    --this->_size;
+    
+    return removenode;
 }
 
 /* Eliminar el primer elemento */
 template <class T>
 Node<T> * LinkedList<T>::remove_front()
 {
-    /* Declarar una variable temporal */
-    Node<T> * node = nullptr;
-    
-    /* Verificar si la lista no está vacía */
-    if (!this->empty()) {
-        
-        /* Guardar una referencia al primer elemento */
-        node = this->_first;
-        
-        /* Actualizamos first al siguiente elemento */
-        this->_first = node->getNext();
-        
-        /* Estableciendo next en nullptr */
-        node->setNext(nullptr);
-        
-        /* Decrementar el tamaño de la lista */
-        --this->_size;
-    }
-    
-    return node;
+    return this->remove(0);
 }
 
 /* Eliminar el último elemento */
 template <class T>
 Node<T> * LinkedList<T>::remove_back()
 {
-    /* Por implementar */
+    return this->remove(this->_size - 1);
 }
 
 /* Eliminar un elemento dado */
 template <class T>
-Node<T> * LinkedList<T>::remove(Node<T> *)
+Node<T> * LinkedList<T>::remove(Node<T> * node)
 {
-    /* Por implementar */
+    return this->remove( this->index(node) );
 }
 
 /* Eliminar todos los elementos de la lista y liberar la memoria ocupada */
 template <class T>
 void LinkedList<T>::clear()
 {
-    /* Por implementar */
+    /* Cuando la lista está vacía */
+    if ( this->empty() ) { return; }
+    
+    /* Obtener una referencia al primer nodo */
+    Node<T> * tmp = this->_first;
+    
+    /* Desplazarse por la lista e ir eliminando los nodos */
+    while (tmp != nullptr) {
+        /* Mover first al siguiente elemento */
+        this->_first = tmp->getNext();
+        
+        /* Liberar la memoria del nodo */
+        delete tmp;
+        
+        /* Apuntar al primero */
+        tmp = this->_first;
+    }
+    
+    /* Establecer el size en 0 */
+    this->_size = 0;
+    
+    /* Establecer first en nullptr */
+    this->_first = nullptr;
 }
 
+/* Obtener el nodo que se encuentra en una posición */
+template  <class T>
+Node<T> *  LinkedList<T>::at(int position) const
+{
+    /* Cuando la lista está vacía o position es inválida */
+    if (this->empty() || (position < 0 || position >= this->_size )) {
+        return nullptr;
+    }
+    
+    /*  Buscar el nodo que se encuentra en position */
+    int pos = 0;
+    
+    /* Obtener una referencia al primer nodo */
+    Node<T> * tmp = this->_first;
+    
+    /* Desplazarse por la lista hasta encontrar el nodo */
+    while (tmp != nullptr && pos++ < position)
+    {
+        tmp = tmp->getNext();
+    }
+    
+    return tmp;
+}
+
+/* Obtener la posición de un nodo */
+template  <class T>
+int LinkedList<T>::index(Node<T> * node) const
+{
+    /* Cuando la lista está vacía o node es nullptr */
+    if (this->empty() || node == nullptr) {
+        return -1;
+    }
+    
+    /* Buscar el valor del nodo y regresar su posición */
+    return this->index( node->getInfo() );
+}
+
+/* Obtener la posición de un valor */
+template  <class T>
+int LinkedList<T>::index(const T & value) const
+{
+    /* Cuando la lista está vacía */
+    if ( this->empty() ) {
+        return -1;
+    }
+    
+    /* Buscar value y regresar su posición */
+    int pos = 0;
+    
+    /* Obtener una referencia al primer nodo */
+    Node<T> * tmp = this->_first;
+    
+    /* Desplazarse por la lista hasta encontrar el value */
+    while (tmp != nullptr && tmp->getInfo() != value)
+    {
+        tmp = tmp->getNext();
+        ++pos;
+    }
+    
+    /* Si el value no se encuentra en la lista */
+    if (pos == this->_size) { return -1; }
+    
+    return pos;
+}
 
 /* Mostrar el contenido de la lista */
 template <class T>
--- Tema_7_Estructuras lineales/LinkedList/LinkedList/main.cpp
+++ Tema_7_Estructuras lineales/LinkedList/LinkedList/main.cpp
@@ -11,64 +11,108 @@
 
 int main(int argc, const char * argv[]) {
     
+    /* Creación de nodos */
+    std::cout << "--- Creación y eliminación de nodos ---" << std::endl;
+    std::cout << std::endl;
+    
     /* Uso de constructor predeterminado Node<T>() */
     Node<int> n1;
-    std::cout << n1 << std::endl;
+    std::cout << "-> Nodo creado con constructor predeterminado: " << n1 << std::endl;
     
     /* Uso de constructor con parámetros Node<T>(T) */
     Node<float> n2(3.14);
-    std::cout << n2 << std::endl;
+    std::cout << "-> Nodo creado con constructor con parámetros: " << n2 << std::endl;
     
     /* Uso de constructor copia Node<T>(Node<T> &) */
     Node<int> n3 = n1;
-    std::cout << n3 << std::endl;
+    std::cout << "-> Nodo creado con constructor copia: " << n3 << std::endl;
     
     /* Crear un nodo con memoria dinámica */
     Node<int> * nodeptr = new Node<int>(10);
-    std::cout << *nodeptr << std::endl;
+    std::cout << "-> Nodo creado con memoria dinámica: " << *nodeptr << std::endl;
     
     /* Liberar la memoria ocupada por el nodo */
+    std::cout << "<- Nodo eliminado con delete: " << *nodeptr << std::endl;
     delete nodeptr;
     
+    /* Trabajo con una LinkedList */
+    std::cout << std::endl;
+    std::cout << "--- Demostración de uso de la LinkedList ---" << std::endl;
+    std::cout << std::endl;
     
     /* Crear una lista */
+    std::cout << "(C) Se crea una lista vacía" << std::endl;
     LinkedList<int> * list = new LinkedList<int>();
     
     /* Determinar si la lista está vacía */
-    std::cout << (list->empty() ? "La lista está vacía" : "La lista tiene elementos") << std::endl;
+    std::cout << (list->empty() ? "(E) La lista está vacía" : "(E) La lista tiene elementos") << std::endl;
     
-    /* Insertar elementos en la lista */
+    /* Insertar un elemento al inicio de la lista */
+    std::cout << "(I) Se inserta el 1 al inicio" << std::endl;
     list->insert_front(1);
-    list->insert_front(2);
+
+    /* Insertar un elemento al final de la lista */
+    std::cout << "(I) Se inserta el 2 al final" << std::endl;
+    list->insert_back(2);
+    
+    /* Insertar un elemento en una posición intermedia de la lista */
+    std::cout << "(I) Se inserta el 5 en la posición 1" << std::endl;
+    list->insert(5,1);
     
     /* Mostrar el contenido de la lista */
+    std::cout << "(L) La lista tiene los siguientes elementos: " << std::endl;
     std::cout << *list << std::endl;
     
     /* Obtener tamaño de la lista */
-    std::cout << "Tamaño de la lista: " << list->size() << " elementos" << std::endl;
+    std::cout << "(S) Tamaño de la lista: " << list->size() << " elementos" << std::endl;
     
-    /* Eliminar un elemento */
+    /* Eliminar un elemento del inicio de la lista */
     Node<int> * node = list->remove_front();
     
-    std::cout << "El nodo eliminado es: " << *node << std::endl;
+    std::cout << "(R) Se eliminó el nodo del inicio: " << *node << std::endl;
     
     /* Liberar la memoria del nodo eliminado */
     delete node;
+    std::cout << "(D) Se liberó la memoria del nodo eliminado" << std::endl;
     
+    /* Eliminar un elemento del final de la lista */
+    node = list->remove_back();
+    
+    std::cout << "(R) Se eliminó el nodo del final: " << *node << std::endl;
+    
+    /* Liberar la memoria del nodo eliminado */
+    delete node;
+    std::cout << "(D) Se liberó la memoria del nodo eliminado" << std::endl;
+    
+    /* Insertar un elemento en una posición intermedia de la lista */
+    std::cout << "(I) Se inserta el 8 en la posición 1" << std::endl;
+    list->insert(8,1);
+    
+    /* Mostrar el contenido de la lista */
+    std::cout << "(L) La lista tiene los siguientes elementos: " << std::endl;
+    std::cout << *list << std::endl;
     
     /* Eliminar un elemento */
-    node = list->remove_front();
-    
-    std::cout << "El nodo eliminado es: " << *node << std::endl;
+    node = list->remove(1);
+    std::cout << "(R) Se eliminó el nodo de la posición 1: " << *node << std::endl;
     
     /* Liberar la memoria del nodo eliminado */
     delete node;
+    std::cout << "(D) Se liberó la memoria del nodo eliminado" << std::endl;
+    
+    /* Obtener tamaño de la lista */
+    std::cout << "(S) Tamaño de la lista: " << list->size() << " elementos" << std::endl;
+    
+    /* Vaciar la lista */
+    list->clear();
+    std::cout << "(V) Se vació la lista"<< std::endl;
     
     /* Determinar si la lista está vacía */
-    std::cout << (list->empty() ? "La lista está vacía" : "La lista tiene elementos") << std::endl;
+    std::cout << (list->empty() ? "(E) La lista está vacía" : "(E) La lista tiene elementos") << std::endl;
     
     /* Liberar la memoria de la lista */
     delete list;
+    std::cout << "(D) Se liberó la memoria de la lista" << std::endl;
     
     
     return 0;
